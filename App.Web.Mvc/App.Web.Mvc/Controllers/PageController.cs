a121ed4d8cea5ee436f24bd808598bcc49536c4b
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2023-06-03 23:01:54 +0300

    Controller eklendi

---
+++ App.Web.Mvc/App.Web.Mvc/Controllers/PageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+    }
+}
